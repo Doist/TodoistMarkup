cb47fe552ef20fe31fd14a66d8335bfb92b87a3a
amix <amix@amix.dk>
2015-06-25 19:20:41 +0100

    Support evernote:// and other URIs

--- src/main/java/com/todoist/markup/Patterns.java
+++ src/main/java/com/todoist/markup/Patterns.java
@@ -6,7 +6,7 @@ class Patterns {
     public static final Pattern HEADER = Pattern.compile("^\\*\\s*");
     public static final Pattern BOLD = Pattern.compile("!!\\s*((?!!!).+?)\\s*!!");
     public static final Pattern ITALIC = Pattern.compile("__\\s*((?!__).+?)\\s*__");
-    public static final Pattern LINK = Pattern.compile("((?:ftp|https?)://[^\\s><\\[\\],]+)(?:\\s+\\(([^)]+)\\))?");
+    public static final Pattern LINK = Pattern.compile("((?:\\w+)://[^\\s><\\[\\],]+)(?:\\s+\\(([^)]+)\\))?");
     public static final Pattern GMAIL = Pattern.compile("\\[\\[gmail=\\s*(.*?)\\s*,\\s*(.*?)\\s*\\]\\]");
     public static final Pattern OUTLOOK = Pattern.compile("\\[\\[outlook=\\s*(.*?)\\s*,\\s*(.*?)\\s*\\]\\]");
     public static final Pattern THUNDERBIRD =
