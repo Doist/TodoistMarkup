2c661dd0401d1623005d692e014d2c653b961a98
Piotr Wilczek <piotrwilczek@gmail.com>
2016-03-06 17:18:05 +0200

    Extend bold and italic pattern

--- src/main/java/com/todoist/markup/Patterns.java
+++ src/main/java/com/todoist/markup/Patterns.java
@@ -3,11 +3,13 @@ package com.todoist.markup;
 import java.util.regex.Pattern;
 
 class Patterns {
+    private static final String PATTERN_BASE = "%s\\s*((?!%s).+?)\\s*%s";
+
     public static final Pattern HEADER = Pattern.compile("^\\*\\s+");
-    public static final Pattern BOLD = Pattern.compile("!!\\s*((?!!!).+?)\\s*!!");
-    public static final Pattern ITALIC = Pattern.compile("_\\s*((?!_).+?)\\s*_");
-    public static final Pattern INLINE_CODE = Pattern.compile("`\\s*((?!`).+?)\\s*`");
-    public static final Pattern CODE_BLOCK = Pattern.compile("```\\s*((?!```).+?)\\s*```", Pattern.DOTALL);
+    public static final Pattern BOLD = Pattern.compile(PATTERN_BASE.replaceAll("%s", "(?:__|\\\\*\\\\*)"));
+    public static final Pattern ITALIC = Pattern.compile(PATTERN_BASE.replaceAll("%s", "[_\\*]"));
+    public static final Pattern INLINE_CODE = Pattern.compile(PATTERN_BASE.replaceAll("%s", "`"));
+    public static final Pattern CODE_BLOCK = Pattern.compile(PATTERN_BASE.replaceAll("%s", "`{3}"), Pattern.DOTALL);
     public static final Pattern LINK = Pattern.compile("((?:[a-zA-Z]+)://[^\\s]+)(?:\\s+\\(([^)]+)\\))?");
     public static final Pattern GMAIL = Pattern.compile("\\[\\[gmail=\\s*(.*?)\\s*,\\s*(.*?)\\s*\\]\\]");
     public static final Pattern OUTLOOK = Pattern.compile("\\[\\[outlook=\\s*(.*?)\\s*,\\s*(.*?)\\s*\\]\\]");
