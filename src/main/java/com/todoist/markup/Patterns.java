e2731c0b8d342a901ac33278c156c7bab73d9093
Piotr Wilczek <piotrwilczek@gmail.com>
2016-03-06 17:24:30 +0200

    Add git add . to bold pattern

--- src/main/java/com/todoist/markup/Patterns.java
+++ src/main/java/com/todoist/markup/Patterns.java
@@ -6,7 +6,7 @@ class Patterns {
     private static final String PATTERN_BASE = "%s\\s*((?!%s).+?)\\s*%s";
 
     public static final Pattern HEADER = Pattern.compile("^\\*\\s+");
-    public static final Pattern BOLD = Pattern.compile(PATTERN_BASE.replaceAll("%s", "(?:__|\\\\*\\\\*)"));
+    public static final Pattern BOLD = Pattern.compile(PATTERN_BASE.replaceAll("%s", "(?:__|!!|\\\\*\\\\*)"));
     public static final Pattern ITALIC = Pattern.compile(PATTERN_BASE.replaceAll("%s", "[_\\*]"));
     public static final Pattern INLINE_CODE = Pattern.compile(PATTERN_BASE.replaceAll("%s", "`"));
     public static final Pattern CODE_BLOCK = Pattern.compile(PATTERN_BASE.replaceAll("%s", "`{3}"), Pattern.DOTALL);
