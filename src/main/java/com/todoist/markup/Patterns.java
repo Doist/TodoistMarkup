a2a881273a46d302624abe279dbe9571fa4490a5
Piotr Wilczek <piotrwilczek@gmail.com>
2016-03-08 11:54:15 +0200

    Refactor patterns

--- src/main/java/com/todoist/markup/Patterns.java
+++ src/main/java/com/todoist/markup/Patterns.java
@@ -3,13 +3,15 @@ package com.todoist.markup;
 import java.util.regex.Pattern;
 
 class Patterns {
-    private static final String PATTERN_BASE = "%s\\s*((?!%s).+?)\\s*%s";
-
     public static final Pattern HEADER = Pattern.compile("^\\*\\s+");
-    public static final Pattern BOLD = Pattern.compile(PATTERN_BASE.replaceAll("%s", "(?:__|!!|\\\\*\\\\*)"));
-    public static final Pattern ITALIC = Pattern.compile(PATTERN_BASE.replaceAll("%s", "[_\\*]"));
-    public static final Pattern INLINE_CODE = Pattern.compile(PATTERN_BASE.replaceAll("%s", "`"));
-    public static final Pattern CODE_BLOCK = Pattern.compile(PATTERN_BASE.replaceAll("%s", "`{3}"), Pattern.DOTALL);
+    // Bold text is wrapped with __ or !! or **
+    public static final Pattern BOLD = Pattern.compile("(?:__|!!|\\*\\*)\\s*((?!(?:__|!!|\\*\\*)).+?)\\s*(?:__|!!|\\*\\*)");
+    // Italic text is wrapped with _ or *
+    public static final Pattern ITALIC = Pattern.compile("[_\\*]\\s*((?![_\\*]).+?)\\s*[_\\*]");
+    // Inline code is wrapped with `
+    public static final Pattern INLINE_CODE = Pattern.compile("`\\s*((?!`).+?)\\s*`");
+    // Code block is wrapped with ```
+    public static final Pattern CODE_BLOCK = Pattern.compile("(?:```)\\s*((?!(?:```)).+?)\\s*(?:```)", Pattern.DOTALL);
     public static final Pattern LINK = Pattern.compile("((?:[a-zA-Z]+)://[^\\s]+)(?:\\s+\\(([^)]+)\\))?");
     public static final Pattern GMAIL = Pattern.compile("\\[\\[gmail=\\s*(.*?)\\s*,\\s*(.*?)\\s*\\]\\]");
     public static final Pattern OUTLOOK = Pattern.compile("\\[\\[outlook=\\s*(.*?)\\s*,\\s*(.*?)\\s*\\]\\]");
